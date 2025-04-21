04db9c73a7c7d8e642743cbd138be05e36b14f78
Luis Gonzalez <lugo50635@gmail.com>
2025-04-21 19:13:22 -0300

    feat: agrega logica de pasarela de pago, envia data a un backend que se encarga de procesar la solicitud

--- src/components/custom/CartSummary.tsx
+++ src/components/custom/CartSummary.tsx
@@ -2,7 +2,28 @@ import { Button } from '../ui/button'
 import { useCart } from '@/hooks/useCart'
 
 function CartSummary () {
-  const { calculateSubtotal, calculateTotal } = useCart()
+  const { calculateSubtotal, calculateTotal, cart } = useCart()
+
+  const handleCheckout = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    
+    try {
+      const response = await fetch('http://localhost:4242/create-checkout-session', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cart }),
+      })
+  
+      if (!response.ok) throw new Error('Error al crear la sesión de pago')
+  
+      const data = await response.json()
+      window.location.href = data.url
+    } catch (error) {
+      console.error('Checkout error:', error)
+    }
+  }
 
   return (
     <div className='w-[30%] h-96 p-3 flex flex-col justify-between border-s-2'>
@@ -11,7 +32,9 @@ function CartSummary () {
       <p>Descuentos: <span className='font-bold'>0</span></p>
       <p>Envio: <span className='font-bold'>Gratis 🆓</span></p>
       <p>Total a pagar: <span className='font-bold'>{calculateTotal()}</span></p>
-      <Button>finalizar compra 🎉</Button>
+      <form onSubmit={handleCheckout}>
+        <Button type='submit'>finalizar compra 🎉</Button>
+      </form>
     </div>
   )
 }
