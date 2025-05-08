071fefe51a76d0403e417a3ef785a9441fe46ed2
Luis Gonzalez <lugo50635@gmail.com>
2025-05-08 15:40:42 -0300

    minor fixes

--- src/components/custom/OrderSuccess.tsx
+++ src/components/custom/OrderSuccess.tsx
@@ -23,7 +23,8 @@ function OrderSuccess () {
     <div className='flex flex-col items-center justify-center gap-5 h-screen'>
       <CircleCheck size={100} color='#47c536' />
       <p className='text-2xl'>La compra se realizo con exito 😄</p>
-      <p className='text-2xl text-center'>El ID de tu orden es: {id}</p>
+      <p className='text-2xl text-center'>El ID de tu orden es:</p>
+      <p className='text-2xl break-all px-10'>{id}</p>
       <Button onClick={handleClick}>volver a los productos</Button>
     </div>
   )
